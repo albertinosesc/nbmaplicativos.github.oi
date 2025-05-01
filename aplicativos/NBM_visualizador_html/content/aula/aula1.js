8e73192ee82f87e146684b135cbfeeed1a1f2136
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-05-01 18:19:18 -0300

    Delete aplicativos/NBM_visualizador_html/content directory

--- aplicativos/NBM_visualizador_html/content/aula/aula1.js
+++
@@ -1,8 +0,0 @@
-registerTextBlock({
-    id: "aula1",
-    content: `
-      <h1>Alunos</h1>
-     <p><span style="color: blue; font-size: 20px;">Acordes Maiores</span> são os mais usados.</p>
-      <p>Acordes Menores</p>
-    `
-  });
--- aplicativos/NBM_visualizador_html/content/text/outros/text000.js
+++
@@ -1,8 +0,0 @@
-
-  registerTextBlock({
-    id: "text50",
-    content: `
-    <p><strong>Nota:</strong> Esta próxima música foi composta por Gervásio em 1999.</p>
-    <p>Ela é ideal para corais e momentos reflexivos.</p>
-    `
-  });
