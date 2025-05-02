cfcfb5ebca9282f94f6fe8a2678536e9dc4e731b
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-05-02 07:05:29 -0300

    Update text18.js

--- aplicativos/NBM_visualizador_html/content/text/text18.js
+++ aplicativos/NBM_visualizador_html/content/text/text18.js
@@ -1,6 +1,6 @@
 //====================================
 //18 - ACORDES MAIORES S/ PESTANA - VIOLÃO - text18.js
-//  ATUALIZAÇÃO:30-04-25
+//  ATUALIZAÇÃO:01-05-25
 //====================================
 registerTextBlock({
     id: "text18",
@@ -13,9 +13,116 @@ Conteúdo Prévio
 </pre>
 
 
+
+<!-- Acorde D -->
+<div class="acorde" data-notas="D3 A3 D4 F#4 D4 A3 D3">
+  <svg width="180" height="220" viewBox="0 0 300 400" xmlns="http://www.w3.org/2000/svg">
+    <text x="150" y="60" font-size="60" text-anchor="middle">D</text>
+    
+                <text x="150" y="60" font-size="60" text-anchor="middle">D</text>
+                <line x1="60" y1="115" x2="240" y2="115" stroke="#000" stroke-width="4"></line>
+                <line x1="60" y1="115" x2="60" y2="340" stroke="#000" stroke-width="2"></line><text x="60" y="95.5" font-size="26" text-anchor="middle">X</text>
+                <line x1="96" y1="115" x2="96" y2="340" stroke="#000" stroke-width="2"></line><text x="96" y="95.5" font-size="26" text-anchor="middle">X</text>
+                <line x1="132" y1="115" x2="132" y2="340" stroke="#000" stroke-width="2"></line><circle cx="132" cy="87.5" r="13" fill="none" stroke="#000" stroke-width="2" class="casa-zero-circulo"></circle>
+                <line x1="168" y1="115" x2="168" y2="340" stroke="#000" stroke-width="2"></line>
+                <line x1="204" y1="115" x2="204" y2="340" stroke="#000" stroke-width="2"></line>
+                <line x1="240" y1="115" x2="240" y2="340" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="115" x2="240" y2="115" stroke="#000" stroke-width="4"></line>
+                <line x1="60" y1="171.25" x2="240" y2="171.25" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="227.5" x2="240" y2="227.5" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="283.75" x2="240" y2="283.75" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="340" x2="240" y2="340" stroke="#000" stroke-width="2"></line>
+                    <circle cx="240" cy="199.375" r="13" fill="#000000" data-key="5-2"></circle>
+                    <text x="240" y="203.375" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="5-2">
+                        2
+                    </text>
+                    <circle cx="204" cy="255.625" r="13" fill="#000000" data-key="4-3"></circle>
+                    <text x="204" y="259.625" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="4-3">
+                        3
+                    </text>
+                    <circle cx="168" cy="199.375" r="13" fill="#000000" data-key="3-2"></circle>
+                    <text x="168" y="203.375" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="3-2">
+                        1
+                    </text>
+  </svg>
+  <button class="playButton">Tocar</button>
+</div>
+
+
+
+<!-- Acorde A -->
+<div class="acorde" data-notas="A2 E3 A3 C#4 E4 C#4 A3 E3 A2">
+  <svg width="180" height="220" viewBox="0 0 300 400" xmlns="http://www.w3.org/2000/svg">
+    <text x="150" y="60" font-size="60" text-anchor="middle">A</text>
+    
+                <text x="150" y="60" font-size="60" text-anchor="middle">A</text>
+                <line x1="60" y1="115" x2="240" y2="115" stroke="#000" stroke-width="4"></line>
+                <line x1="60" y1="115" x2="60" y2="340" stroke="#000" stroke-width="2"></line><text x="60" y="95.5" font-size="26" text-anchor="middle">X</text>
+                <line x1="96" y1="115" x2="96" y2="340" stroke="#000" stroke-width="2"></line><circle cx="96" cy="87.5" r="13" fill="none" stroke="#000" stroke-width="2" class="casa-zero-circulo"></circle>
+                <line x1="132" y1="115" x2="132" y2="340" stroke="#000" stroke-width="2"></line>
+                <line x1="168" y1="115" x2="168" y2="340" stroke="#000" stroke-width="2"></line>
+                <line x1="204" y1="115" x2="204" y2="340" stroke="#000" stroke-width="2"></line>
+                <line x1="240" y1="115" x2="240" y2="340" stroke="#000" stroke-width="2"></line><circle cx="240" cy="87.5" r="13" fill="none" stroke="#000" stroke-width="2" class="casa-zero-circulo"></circle>
+                <line x1="60" y1="115" x2="240" y2="115" stroke="#000" stroke-width="4"></line>
+                <line x1="60" y1="171.25" x2="240" y2="171.25" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="227.5" x2="240" y2="227.5" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="283.75" x2="240" y2="283.75" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="340" x2="240" y2="340" stroke="#000" stroke-width="2"></line>
+                    <circle cx="168" cy="199.375" r="13" fill="#000000" data-key="3-2"></circle>
+                    <text x="168" y="203.375" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="3-2">
+                        2
+                    </text>
+                    <circle cx="132" cy="199.375" r="13" fill="#000000" data-key="2-2"></circle>
+                    <text x="132" y="203.375" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="2-2">
+                        1
+                    </text>
+                    <circle cx="204" cy="199.375" r="13" fill="#000000" data-key="4-2"></circle>
+                    <text x="204" y="203.375" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="4-2">
+                        3
+                    </text>
+  </svg>
+  <button class="playButton">Tocar</button>
+</div>
+
+
+
+<!-- Acorde E -->
+<div class="acorde" data-notas="E2 B2 E3 G#3 B3 E4 B3 G#3 E3 B2 E2">
+  <svg width="180" height="220" viewBox="0 0 300 400" xmlns="http://www.w3.org/2000/svg">
+    <text x="150" y="60" font-size="60" text-anchor="middle">E</text>
+    
+                <text x="150" y="60" font-size="60" text-anchor="middle">E</text>
+                <line x1="60" y1="115" x2="240" y2="115" stroke="#000" stroke-width="4"></line>
+                <line x1="60" y1="115" x2="60" y2="340" stroke="#000" stroke-width="2"></line><circle cx="60" cy="87.5" r="13" fill="none" stroke="#000" stroke-width="2" class="casa-zero-circulo"></circle>
+                <line x1="96" y1="115" x2="96" y2="340" stroke="#000" stroke-width="2"></line>
+                <line x1="132" y1="115" x2="132" y2="340" stroke="#000" stroke-width="2"></line>
+                <line x1="168" y1="115" x2="168" y2="340" stroke="#000" stroke-width="2"></line>
+                <line x1="204" y1="115" x2="204" y2="340" stroke="#000" stroke-width="2"></line><circle cx="204" cy="87.5" r="13" fill="none" stroke="#000" stroke-width="2" class="casa-zero-circulo"></circle>
+                <line x1="240" y1="115" x2="240" y2="340" stroke="#000" stroke-width="2"></line><circle cx="240" cy="87.5" r="13" fill="none" stroke="#000" stroke-width="2" class="casa-zero-circulo"></circle>
+                <line x1="60" y1="115" x2="240" y2="115" stroke="#000" stroke-width="4"></line>
+                <line x1="60" y1="171.25" x2="240" y2="171.25" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="227.5" x2="240" y2="227.5" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="283.75" x2="240" y2="283.75" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="340" x2="240" y2="340" stroke="#000" stroke-width="2"></line>
+                    <circle cx="96" cy="199.375" r="13" fill="#000000" data-key="1-2"></circle>
+                    <text x="96" y="203.375" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="1-2">
+                        2
+                    </text>
+                    <circle cx="132" cy="199.375" r="13" fill="#000000" data-key="2-2"></circle>
+                    <text x="132" y="203.375" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="2-2">
+                        3
+                    </text>
+                    <circle cx="168" cy="143.125" r="13" fill="#000000" data-key="3-1"></circle>
+                    <text x="168" y="147.125" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="3-1">
+                        1
+                    </text>
+  </svg>
+  <button class="playButton">Tocar</button>
+</div>
+
 <!-- Acorde C -->
 <div class="acorde" data-notas="C3 E3 G3 C4 E4 C4 G3 E3 C3">
-  <svg width="300" height="400" viewBox="0 0 300 400" xmlns="http://www.w3.org/2000/svg">
+  <svg width="180" height="220" viewBox="0 0 300 400" xmlns="http://www.w3.org/2000/svg">
     <text x="150" y="60" font-size="60" text-anchor="middle">C</text>
     
                 <text x="150" y="60" font-size="60" text-anchor="middle">C</text>
@@ -41,6 +148,42 @@ Conteúdo Prévio
                     </text>
                     <circle cx="96" cy="255.625" r="13" fill="#000000" data-key="1-3"></circle>
                     <text x="96" y="259.625" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="1-3">
+                        3
+                    </text>
+  </svg>
+  <button class="playButton">Tocar</button>
+</div>
+
+
+
+<!-- Acorde G -->
+<div class="acorde" data-notas="G2 B2 D3 G3 B3 G4 B3 G3 D3 B2 G2">
+  <svg width="180" height="220" viewBox="0 0 300 400" xmlns="http://www.w3.org/2000/svg">
+    <text x="150" y="60" font-size="60" text-anchor="middle">G</text>
+    
+                <text x="150" y="60" font-size="60" text-anchor="middle">G</text>
+                <line x1="60" y1="115" x2="240" y2="115" stroke="#000" stroke-width="4"></line>
+                <line x1="60" y1="115" x2="60" y2="340" stroke="#000" stroke-width="2"></line>
+                <line x1="96" y1="115" x2="96" y2="340" stroke="#000" stroke-width="2"></line>
+                <line x1="132" y1="115" x2="132" y2="340" stroke="#000" stroke-width="2"></line><circle cx="132" cy="87.5" r="13" fill="none" stroke="#000" stroke-width="2" class="casa-zero-circulo"></circle>
+                <line x1="168" y1="115" x2="168" y2="340" stroke="#000" stroke-width="2"></line><circle cx="168" cy="87.5" r="13" fill="none" stroke="#000" stroke-width="2" class="casa-zero-circulo"></circle>
+                <line x1="204" y1="115" x2="204" y2="340" stroke="#000" stroke-width="2"></line><circle cx="204" cy="87.5" r="13" fill="none" stroke="#000" stroke-width="2" class="casa-zero-circulo"></circle>
+                <line x1="240" y1="115" x2="240" y2="340" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="115" x2="240" y2="115" stroke="#000" stroke-width="4"></line>
+                <line x1="60" y1="171.25" x2="240" y2="171.25" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="227.5" x2="240" y2="227.5" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="283.75" x2="240" y2="283.75" stroke="#000" stroke-width="2"></line>
+                <line x1="60" y1="340" x2="240" y2="340" stroke="#000" stroke-width="2"></line>
+                    <circle cx="60" cy="255.625" r="13" fill="#000000" data-key="0-3"></circle>
+                    <text x="60" y="259.625" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="0-3">
+                        2
+                    </text>
+                    <circle cx="96" cy="199.375" r="13" fill="#000000" data-key="1-2"></circle>
+                    <text x="96" y="203.375" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="1-2">
+                        1
+                    </text>
+                    <circle cx="240" cy="255.625" r="13" fill="#000000" data-key="5-3"></circle>
+                    <text x="240" y="259.625" font-size="14" fill="#ffffff" text-anchor="middle" dominant-baseline="middle" data-key="5-3">
                         3
                     </text>
   </svg>
