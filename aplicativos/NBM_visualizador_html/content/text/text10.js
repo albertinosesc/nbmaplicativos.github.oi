939ed81009e485c2c03d1166f7b46f87f0143124
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-05-14 12:11:56 -0300

    Update text10.js

--- aplicativos/NBM_visualizador_html/content/text/text10.js
+++ aplicativos/NBM_visualizador_html/content/text/text10.js
@@ -1,24 +1,24 @@
 //====================================
 //10 - Compasso - text10.js
-//  ATUALIZAÇÃO:30-04-25
+//  ATUALIZAÇÃO:14-05-25
 //====================================
 registerTextBlock({
     id: "text10",
     content: `
-<h1>9 - BARRA SIMPLES</h1>
+<h1>10 - COMPASSO</h1>
 
 <pre>
 Conteúdo Prévio
 9 - Barra Simples
 </pre>
-<p>São os espaços entre as barras simples</p>
+<p1>São os espaços entre as barras simples</p1>
 
-<p style="font-size: 20px;">
+<p1 style="font-size: 20px;">
   | <span style="color: red;">compasso</span> |
   <span style="color: red;">compasso</span> |
   <span style="color: red;">compasso</span> |
   <span style="color: red;">compasso</span> |
-</p>
+</p1>
 
     `
   });
