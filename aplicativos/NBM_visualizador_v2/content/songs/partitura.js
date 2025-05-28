cec7ce7184d3274b081cd64dc46aa487ec67634f
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-05-28 00:22:13 -0300

    Add files via upload

---
+++ aplicativos/NBM_visualizador_v2/content/songs/partitura.js
@@ -0,0 +1,13 @@
+const musicList = [
+    { name: "Teste audio 1", link: "music.html#music2674" },
+    { name: "015 - Boil the breakfast early", link: "music.html#music015" },
+    { name: "016 - The bonnie boy", link: "music.html#music016" },
+    { name: "017 - Bonnie Kate", link: "music.html#music017" },
+    { name: "018 - The boy in the gap", link: "music.html#music018" },
+    { name: "019 - The boys of Ballisadare", link: "music.html#music019" },
+    { name: "020 - The boys of Malin", link: "music.html#music020" },
+    { name: "021 - The boys of the lough", link: "music.html#music021" },
+    { name: "022 - Brenda Stubbert's", link: "music.html#music022" },
+    { name: "023 - The broken pledge", link: "music.html#music023" },
+    { name: "024 - Brosnan's", link: "music.html#music024" }
+];
