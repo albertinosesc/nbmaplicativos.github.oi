356daeffc5f5244a1a624b0f427975262a3d93e9
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-05-28 00:22:51 -0300

    Add files via upload

---
+++ aplicativos/NBM_visualizador_v2/content/text/text/texto.js
@@ -0,0 +1,14 @@
+const textList = [
+    { name: "1 - Nomes Das Notas Musicais", link: "text.html#text1" },
+    { name: "2 - Relógio Musical", link: "text.html#text2" },
+    { name: "3 - Letra Do Alfabeto", link: "text.html#text3" },
+    { name: "4 - Cifras", link: "text.html#text4" },
+    { name: "5 - Diagrama De Acorde - Violão", link: "text.html#text5" },
+    { name: "6 - Cordas (Diagrama) - Violão", link: "text.html#text6" },
+    { name: "7 - Casa (Diagrama) - Violão", link: "text.html#text7" },
+    { name: "8 - Gráfico Harmônico", link: "text.html#text8" },
+    { name: "9 - Barra Simples", link: "text.html#text9" },
+    { name: "10 - Compasso", link: "text.html#text10" },
+    { name: "11 - Barra Final", link: "text.html#text11" },
+    { name: "12 - Signo De Compasso (Gráfico Harmônico)", link: "text.html#text12" }
+];
