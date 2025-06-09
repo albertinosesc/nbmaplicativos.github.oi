8c2e07a1de148414dd08d9f946afbbe6f4511759
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-06-08 23:22:42 -0300

    Update partitura.js

--- aplicativos/NBM_visualizador_v2/content/songs/partitura.js
+++ aplicativos/NBM_visualizador_v2/content/songs/partitura.js
@@ -9,5 +9,7 @@ const musicList = [
     { name: "021 - The boys of the lough", link: "music.html#music021" },
     { name: "022 - Brenda Stubbert's", link: "music.html#music022" },
     { name: "023 - The broken pledge", link: "music.html#music023" },
-    { name: "024 - Brosnan's", link: "music.html#music024" }
+    { name: "024 - Brosnan's", link: "music.html#music024" },
+    { name: "23579 - Adaptação refrão CCB 438 parte A", link: "music.html#music23579" }
+    
 ];
