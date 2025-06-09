a6886b255321e45c39cf85675f5b0f9083ecd8e0
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-06-08 23:23:18 -0300

    Add files via upload

---
+++ aplicativos/NBM_visualizador_v2/content/songs/music23579.js
@@ -0,0 +1,13 @@
+registerSong({
+  id: 'music23579',
+  title: 'Adaptação refrão CCB 438 parte A',
+  abc: `X:1
+T:23579 - Adaptação: refrão CCB-438 parte A
+Q:1/4=80
+M:4/4
+L:1/4
+K:E
+|:efga|zegz|fezz:|
+w:Per-to es-ta o Sal-va-dor
+w:dos que o tem por seu Pas-tor.`
+});
