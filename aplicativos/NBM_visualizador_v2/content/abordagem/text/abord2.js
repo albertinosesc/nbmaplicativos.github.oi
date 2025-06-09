653789895f6efa82e678927973bbdca3b0503b8b
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-06-08 23:20:57 -0300

    Update abord2.js

--- aplicativos/NBM_visualizador_v2/content/abordagem/text/abord2.js
+++ aplicativos/NBM_visualizador_v2/content/abordagem/text/abord2.js
@@ -1,15 +1,42 @@
-//====================================
-//2 - RELÓGIO MUSICAL - text2.js
-//  ATUALIZAÇÃO:30-04-25
-//====================================
 registerAbordagem({
-    id: "abord2",
-    content: `
-<h1>2 - Teste - abord2</h1>
-<p2>última atualização beto</p2>
-<br>
-abord2
+  id: "abord2",
+  content: `
+<small>última atualização - 07-06-25</small>
+<h2>2 - Leitura de Notas na Partitura</h2>
+<pre>
+Abordagem: Leitura de Notas na Partitura
 
-    `
-  });
+Objetivo Geral:
+Desenvolver a habilidade de leitura fluente de notas na partitura, com foco
+    na identificação rápida e execução regular, usando ritmo fixo de semínima.
+    
+
+Princípios da Atividade:
+• Todas as notas são executadas com duração de 1 tempo.
+• Em notas simultâneas com durações diferentes, prevalece a menor duração, que
+    são tratadas como semínimas. a de maior duração tem ter o valor relativo em 
+    relação a de menor duração.
+• O foco está na precisão da leitura e manutenção do pulso.
+
+Níveis Progressivos:
+    
+• Meta: ler e tocar com precisão e fluência.
+• Desenvolve coordenação e leitura vertical.
+• Exige reconhecimento rápido e adaptação no instrumento.
+• Expande a leitura para toda a extensão do instrumento conhecido.
+
+Sugestões Didáticas:
+• Usar metrônomo desde o início para firmar o pulso.
+• Fazer leitura silenciosa (mental) antes de tocar.
+• Alternar leitura cantada e tocada.
+
+Avaliações:
+• Precisão na identificação das notas.
+• Capacidade de manter o tempo constante.
+• Fluência na leitura simultânea e reconhecimento de claves.
+
+</pre>
+`
+});
+
 
