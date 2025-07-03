35037a9d08a53ea4eb9bb7ff457dc146021f5608
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-07-03 19:20:22 -0300

    Update quant5.js

--- aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/nivel1/score/1_monopentagramatica/quant/quant5.js
+++ aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/nivel1/score/1_monopentagramatica/quant/quant5.js
@@ -1,8 +1,8 @@
 window.nivel4 = [
-  { nome: "12345", arquivo: "js/nivel1/score/1_monopentagramatica/nota/nota12345.js" },
-  { nome: "12356", arquivo: "js/nivel1/score/1_monopentagramatica/nota/nota12356.js" },
-  //{ nome: "23567b", arquivo: "js/nivel1/score/1_monopentagramatica/nota/nota23567b.js" },
-  //{ nome: "56123", arquivo: "js/nivel1/score/1_monopentagramatica/nota/nota56123.js" },
-  //{ nome: "56712", arquivo: "js/nivel1/score/1_monopentagramatica/nota/nota56712.js" },
-  { nome: "67123", arquivo: "js/nivel1/score/1_monopentagramatica/nota/nota67123.js" }
+  { nome: "12345", arquivo: "js/nivel1/score/1_monopentagramatica/nota/5nota12345.js" },
+  { nome: "12356", arquivo: "js/nivel1/score/1_monopentagramatica/nota/5nota12356.js" },
+  //{ nome: "23567b", arquivo: "js/nivel1/score/1_monopentagramatica/nota/5nota23567b.js" },
+  //{ nome: "56123", arquivo: "js/nivel1/score/1_monopentagramatica/nota/5nota56123.js" },
+  //{ nome: "56712", arquivo: "js/nivel1/score/1_monopentagramatica/nota/5nota56712.js" },
+  { nome: "67123", arquivo: "js/nivel1/score/1_monopentagramatica/nota/5nota67123.js" }
 ];
