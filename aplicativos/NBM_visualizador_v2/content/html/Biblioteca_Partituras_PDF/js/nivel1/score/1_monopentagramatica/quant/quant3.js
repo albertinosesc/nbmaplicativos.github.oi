30b248f61ffcd4dd8c213feac8cf4149d73ca179
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-07-03 19:21:35 -0300

    Update quant3.js

--- aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/nivel1/score/1_monopentagramatica/quant/quant3.js
+++ aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/nivel1/score/1_monopentagramatica/quant/quant3.js
@@ -1,8 +1,8 @@
 
 //quanatidade
 window.nivel4 = [
-  { nome: "123", arquivo: "js/nivel1/score/1_monopentagramatica/nota/nota123.js" },
-  { nome: "356", arquivo: "js/nivel1/score/1_monopentagramatica/nota/nota356.js" }
+  { nome: "123", arquivo: "js/nivel1/score/1_monopentagramatica/nota/3nota123.js" },
+  { nome: "356", arquivo: "js/nivel1/score/1_monopentagramatica/nota/3nota356.js" }
 
   
 ];
