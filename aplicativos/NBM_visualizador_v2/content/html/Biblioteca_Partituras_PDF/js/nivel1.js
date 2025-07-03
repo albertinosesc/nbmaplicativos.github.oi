f33978100f65c920a5748b3e3b7b50acbb385787
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-07-03 19:13:03 -0300

    Add files via upload

---
+++ aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/nivel1.js
@@ -0,0 +1,9 @@
+//nivel1.js
+window.nivel1 = [
+  { nome: "Compositor", arquivo: "js/nivel1/compositor/compositor.js" },
+  { nome: "Instrumento", arquivo: "js/nivel1/instrumento/instrumentos.js" },
+  { nome: "Livros", arquivo: "js/nivel1/livro/livro.js" },
+  { nome: "Score", arquivo: "js/nivel1/score/score.js" }
+
+
+];
