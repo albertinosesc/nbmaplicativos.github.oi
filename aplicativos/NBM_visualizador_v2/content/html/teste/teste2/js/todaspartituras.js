90eb56d0ff5626c07e70ad089eef67a0940bdc5a
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-07-03 14:52:07 -0300

    Add files via upload

---
+++ aplicativos/NBM_visualizador_v2/content/html/teste/teste2/js/todaspartituras.js
@@ -0,0 +1,10 @@
+//todaspartituras.js
+window.musicas = [
+  { name: "23", file: "pdf/2674 - Ave Maria - Gounod Charles.pdf", composer: "Arr Gilbert DeBenedetti", level: "A" },
+  { name: "23", file: "midi/2674 - Ave Maria - Gounod Charles.mid", composer: "Arr Gilbert DeBenedetti", level: "A" },
+  { name: "23", file: "audio/2674 - Ave Maria - Gounod Charles.mp3", composer: "Arr Gilbert DeBenedetti", level: "A" },
+  { name: "23", file: "abc/2674 - Ave Maria - Gounod Charles.abc", composer: "Arr Gilbert DeBenedetti", level: "A" }
+];
+
+
+
