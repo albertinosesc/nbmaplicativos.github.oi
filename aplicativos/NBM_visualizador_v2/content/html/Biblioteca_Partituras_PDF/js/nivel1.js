b972adaba79666d0318398dfd0b142e31e4eaf05
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-07-03 19:12:38 -0300

    Delete aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/nivel1.js

--- aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/nivel1.js
+++
@@ -1,6 +0,0 @@
-//nivel1.js
-window.nivel1 = [
-  { nome: "Instrumento", arquivo: "js/nivel1/instrumento/instrumentos.js" },
-  { nome: "Score", arquivo: "js/nivel1/score/score.js" },
-  { nome: "Compositor", arquivo: "js/nivel1/compositor/compositor.js" }
-];
