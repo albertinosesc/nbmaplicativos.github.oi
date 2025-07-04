738864514b03743d5306fee06260853fd2624086
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-07-03 23:50:15 -0300

    Update nivel1.js

--- aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/nivel1.js
+++ aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/nivel1.js
@@ -3,7 +3,9 @@ window.nivel1 = [
   { nome: "Compositor", arquivo: "js/nivel1/compositor/compositor.js" },
   { nome: "Instrumento", arquivo: "js/nivel1/instrumento/instrumentos.js" },
   { nome: "Livros", arquivo: "js/nivel1/livro/livro.js" },
-  { nome: "Score", arquivo: "js/nivel1/score/score.js" }
+  { nome: "Score", arquivo: "js/nivel1/score/score.js" },
+  { nome: "dificuldade", arquivo: "js/nivel1/dificuldade/dificuldade.js" },
 
 
 ];
+
