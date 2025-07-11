7616799fb5e2f93292b6545801784bb66939ab24
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-07-11 18:09:23 -0300

    Update todaspartiturasmp3.js

--- aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/todaspartiturasmp3.js
+++ aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/todaspartiturasmp3.js
@@ -1,6 +1,56 @@
-//todaspartiturasmp3.js
-window.musicas = [
 
-  { name: "23", file: "mp3/2674 - Ave Maria - Gounod Charles.mp3", composer: "Arr Gilbert DeBenedetti", level: "A" },
+// js/todaspartiturasmp3.js
+window.musicas = window.musicas || [];
+window.musicas.push(
+
+    
+  { id: "23581", name: "Hot Cross Buns", file: "mp3/23581 - Hot Cross Buns - Arr Gilbert DeBenedetti - FirstPiecesA.mp3" },
+  { id: "23582", name: "Mary Had a Little Lamb", file: "mp3/23582 - Mary Had a Little Lamb - Arr Gilbert DeBenedetti - FirstPiecesA.mp3" },
+  { id: "23583", name: "Au Clair de la Lune", file: "mp3/23583 - Au Clair de la Lune - Arr Gilbert DeBenedetti - FirstPiecesA.mp3" },
+  { id: "23584", name: "Rain, Rain, Go Away", file: "mp3/23584 - Rain, Rain, Go Away - Arr Gilbert DeBenedetti - FirstPiecesA.mp3" },
+  { id: "23585", name: "Jingle Bells", file: "mp3/23585 - Jingle Bells - J. Pierpont - FirstPiecesA.mp3" },
+  { id: "23586", name: "Aura Lee", file: "mp3/23586 - Aura Lee - George R. Poulton - FirstPiecesA.mp3" },
+  { id: "23587", name: "Do, Do l'Enfant Do", file: "mp3/23587 - Do, Do l'Enfant Do - Arr Gilbert DeBenedetti - FirstPiecesA.mp3" },
+  { id: "23588", name: "A Tisket, a Tasket", file: "mp3/23588 - A Tisket, a Tasket - Arr Gilbert DeBenedetti - FirstPiecesA.mp3" },
+  { id: "23589", name: "Ode to Joy", file: "mp3/23589 - Ode to Joy - Beethoven - FirstPiecesA.mp3" },
+  { id: "23590", name: "Good King Wenceslas", file: "mp3/23590 - Good King Wenceslas - Arr Gilbert DeBenedetti - FirstPiecesA.mp3" },
+  { id: "23591", name: "Sweetly Sings the Donkey", file: "mp3/23591 - Sweetly Sings the Donkey - Arr Gilbert DeBenedetti - FirstPiecesA.mp3" },
+  { id: "23592", name: "Go Tell Aunt Rhodie", file: "mp3/23592 - Go Tell Aunt Rhodie - Arr Gilbert DeBenedetti - FirstPiecesA.mp3" },
+  { id: "23593", name: "Row, Row, Row Your Boat", file: "mp3/23593 - Row, Row, Row Your Boat - Arr Gilbert DeBenedetti - A.mp3" },
+  { id: "23594", name: "Camptown Races", file: "mp3/23594 - Camptown Races - Stephen Foster - A.mp3" },
+  { id: "23597", name: "I Saw Three Ships", file: "mp3/23597 - I Saw Three Ships - Arr Gilbert DeBenedetti - A.mp3" },
+  { id: "23598", name: "Banks of the Ohio", file: "mp3/23598 - Banks of the Ohio - Traditional - A.mp3" },
+  { id: "23599", name: "In the Field Stood a Birch Tree", file: "mp3/23599 - In the Field Stood a Birch Tree - Tradtional Russian - A.mp3" },
+  { id: "23603", name: "Away in the Deep Forest", file: "mp3/23603 - Away in the Deep Forest - Arr Gilbert DeBenedetti - A.mp3" },
+  { id: "23606", name: "Fais Dodo", file: "mp3/23606 - Fais Dodo - Arr Gilbert DeBenedetti - A.mp3" },
+  { id: "23645", name: "Bullock Cart", file: "mp3/23645 - Bullock Cart - Arr Gilbert DeBenedetti - FirstPiecesA.mp3" },
+  { id: "23646", name: "Fathers Whiskers", file: "mp3/23646 - Fathers Whiskers - Arr Gilbert DeBenedetti - FirstPiecesA.mp3" },
+  { id: "23647", name: "Oats Peas Beans and Barley Grow", file: "mp3/23647 - Oats Peas Beans and Barley Grow - Traditional - FirstPiecesA.mp3" },
+  { id: "23651", name: "Nobody Likes Me", file: "mp3/23651 - Nobody Likes Me - Arr Gilbert DeBenedetti - A.mp3" },
+  { id: "23595", name: "For Health and Strength", file: "mp3/23595 - For Health and Strength - Arr Gilbert DeBenedetti - PrimerA.mp3" },
+  { id: "23596", name: "We Three Kings", file: "mp3/23596 - We Three Kings - John Henry Hopkins - PrimerA.mp3" },
+  { id: "23696", name: "Frère Jacques", file: "mp3/23696 - Frère Jacques - Arr Gilbert DeBenedetti - PrimerB.mp3" },
+  { id: "23697", name: "My Palm Leaf Hut Bahay Kubo", file: "mp3/23697 - My Palm Leaf Hut Bahay Kubo - Arr Gilbert DeBenedetti - PrimerB.mp3" },
+  { id: "23600", name: "Bridal March", file: "mp3/23600 - Bridal March - Richard Wagne - PimerA.mp3" },
+  { id: "23601", name: "Mexican Hat Dance", file: "mp3/23601 - Mexican Hat Dance - Arr Gilbert DeBenedetti - PrimerA.mp3" },
+  { id: "23602", name: "Theme to Swan Lake", file: "mp3/23602 - Theme to Swan Lake - Pyotr Ilyich Tchaikovsky - PrimerA.mp3" },
+  { id: "23604", name: "Jolly Old Saint Nicholas", file: "mp3/23604 - Jolly Old Saint Nicholas - Arr Gilbert DeBenedetti - PrimerA.mp3" },
+  { id: "23605", name: "Once a Small Green Mousie", file: "mp3/23605 - Once a Small Green Mousie - Arr Gilbert DeBenedetti - PrimerA.mp3" },
+  { id: "23607", name: "Auld Lang Syne", file: "mp3/23607 - Auld Lang Syne - Arr Gilbert DeBenedetti - PrimerA.mp3" },
+
+  { id: "23648", name: "Everywhere We Go", file: "mp3/23648 - Everywhere We Go - Arr Gilbert DeBenedetti - FirstPiecesB.mp3" },
+  { id: "23649", name: "Lavender's Blue", file: "mp3/23649 - Lavender's Blue - Traditional English - FirstPiecesB.mp3" },
+  { id: "23650", name: "Long Long Ago", file: "mp3/23650 - Long Long Ago - Thomas Haynes Bailey - FirstPiecesB.mp3" },
   
-];
+  { id: "23652", name: "Plaisir D'amour", file: "mp3/23652 - Plaisir D'amour - Jean-Paul-ègide Martini - FirstPiecesB.mp3" },
+  
+  { id: "23654", name: "Shabbat Shalom", file: "mp3/23654 - Shabbat Shalom - Traditional Jewish - FirstPiecesB.mp3" },
+  { id: "23655", name: "This Little Light of Mine", file: "mp3/23655 - This Little Light of Mine - Arr Gilbert DeBenedetti - FirstPiecesB.mp3" }
+
+
+
+
+    
+    
+    
+);
