08a9d915e3589987e3ebf9ac9cbd6ede7d4ba317
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2025-07-11 21:35:26 -0300

    Update todaspartiturasmid.js

--- aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/todaspartiturasmid.js
+++ aplicativos/NBM_visualizador_v2/content/html/Biblioteca_Partituras_PDF/js/todaspartiturasmid.js
@@ -1,7 +1,10 @@
 //todaspartiturasmid.js
-window.musicas = [
-
+window.musicas = window.musicas || [];
+window.musicas.push(
   { name: "100", file: "mid/100 - Ave Maria - Gounod Charles.mid", composer: "Arr Gilbert DeBenedetti", level: "A" },
+{ id: "23589", name: "Ode to Joy", file: "mid/23589 - Ode to Joy - Beethoven - A.mid"},
+{ id: "23581", name: "Hot Cross Buns", file: "mid/23581 - Hot Cross Buns - Arr Gilbert DeBenedetti - FirstPiecesA.mid" }
 
-];
+
+);
 
