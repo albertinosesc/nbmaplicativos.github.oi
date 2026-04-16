7364a1045c973ffafca706ed07da18f95283336b
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2026-04-16 20:37:05 -0300

    Add minor chord definitions for C in JavaScript

---
+++ aplicativos/piano/markdown/acorde/02_menor_c.js
@@ -0,0 +1,38 @@
+// ============================================
+// ACORDES MAIORES - TOM DE DÓ (C)
+// ============================================
+
+const ACORDES_MENOR_C = {
+
+    'y': {
+    nome: 'Cm',
+    cordas: [-1,3,5,5,4,3],
+    dedos: ['','1','3','4','2','1'],
+    pestana: [1,2,3,4,5],
+    casaInicial: 3,
+    baixo: 'C'
+},
+    'r': {
+        nome: 'r',
+        cordas: [-1,-1,2,4,5,3],
+        dedos: ['','','1','3','4','2'],
+        pestana: [2,3,4,5],
+        casaInicial: 2,
+        baixo: 'R'
+    },
+
+
+
+
+
+
+    
+
+    
+};
+
+// Exportar
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = ACORDES_MENOR_C;
+}
+
