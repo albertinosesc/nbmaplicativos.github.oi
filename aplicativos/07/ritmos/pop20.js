ea0d7ecf2207c122e05505eea412f9c2121157da
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2026-06-26 18:27:50 -0300

    Add files via upload

---
+++ aplicativos/07/ritmos/pop20.js
@@ -0,0 +1,454 @@
+(() => {
+    // ==========================================
+    // 1. ESTILO: POP PIANO LITE SOLO PIANO (p1 a p9) - 4/4
+    // ==========================================
+    REGISTRO_RITMOS.registrar({
+        categoria: "Pop 20",
+        subcategoria: "Ballad",
+        style: "Pop Piano Lite Solo Piano",
+        variacoes: {
+            "Pop 1": {
+                alias: "p1",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]3 {RH5} {RH3}{RH1} d2", desenhoLh: "{LH} {LH5} {LH}4- {LH} {LH}" }]
+            },
+            "Pop 2": {
+                alias: "p2",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH3}{RH1}]3 {RH1} [{RH5}{RH1}]2 {RH5}2", desenhoLh: "{LH}4- {LH} E,2 {LH}" }]
+            },
+            "Pop 3": {
+                alias: "p3",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH5}d]3 [{RH5}{RH1}]- [{RH5}{RH1}]2 {RH3}{RH5}", desenhoLh: "{LH}4 E, {LH5}3" }]
+            },
+            "Pop 4": {
+                alias: "p4",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH_A}2 {RH3}[{RH_A}{RH5}]- [{RH_A}{RH5}]{RH3} z2", desenhoLh: "[{LH}{LH5}] {LH}3- {LH}2 {LH}2" }]
+            },
+            "Pop 5": {
+                alias: "p5",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH5}2 {RH3}{RH_A}- {RH_A}{RH5} z2", desenhoLh: "[{LH}{LH5}] {LH}3 z4" }]
+            },
+            "Pop 6": {
+                alias: "p6",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH_A} {RH5}2 [{RH_A}{RH5}]- [{RH_A}{RH5}]d z2", desenhoLh: "[{LH}{LH}]4 {LH5}4" }]
+            },
+            "Pop 7": {
+                alias: "p7",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH5}{RH3}{RH5}{RH_A}- {RH_A}{RH3} {RH5}2", desenhoLh: "[{LH}{LH5}]4 {LH}2 z2" }]
+            },
+            "Pop 8": {
+                alias: "p8",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH3}{RH5}{RH1}{RH5}- {RH5}{RH1} {RH5}2", desenhoLh: "[{LH}]6 z2" }]
+            },
+            "Pop 9": {
+                alias: "p9",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH3} {RH1}2 {RH5}- {RH5}{RH1} z2", desenhoLh: "[{LH}]4 z4" }]
+            }
+        }
+    });
+
+    // ==========================================
+    // 2. ESTILO: SKY OPENS GOSPEL PIANO BALLAD (p10 a p23) - 4/4
+    // ==========================================
+    REGISTRO_RITMOS.registrar({
+        categoria: "Pop 20",
+        subcategoria: "Ballad",
+        style: "Sky Opens Gospel Piano Ballad",
+        variacoes: {
+            "Pop 10": {
+                alias: "p10",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 {RH5}{RH3} {RH3}2 [{RH}]2", desenhoLh: "[{LH5}{LH}]3 {LH5} {LH5}2 {LH3}2" }]
+            },
+            "Pop 11": {
+                alias: "p11",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]4 [{RH_A}]2 [{RH_A}]2", desenhoLh: "[{LH5}{LH}]3 {LH3} {LH}2 {LH}2" }]
+            },
+            "Pop 12": {
+                alias: "p12",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH3}]4 {RH5}2 z2", desenhoLh: "[{LH3}{LH5}]3 {LH5} {LH}{LH3}[{LH5}{LH}]{LH5}" }]
+            },
+            "Pop 13": {
+                alias: "p13",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 {RH3}2 {RH1}2 z2", desenhoLh: "{LH}6 z2" }]
+            },
+            "Pop 14": {
+                alias: "p14",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 {RH3}4 {RH5}/{RH_G}/[{RH1}]", desenhoLh: "[{LH5}{LH}]3 {LH} {LH}2 {LH5}/[{LH}]" }]
+            },
+            "Pop 15": {
+                alias: "p15",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 {RH1}[{RH}] [{RH}]2 [{RH}]>{RH3}", desenhoLh: "[{LH3}{LH5}]3 {LH3} {LH3}2 {LH3}>=F," }]
+            },
+            "Pop 16": {
+                alias: "p16",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 {RH5}4- {RH5}[{RH3}]", desenhoLh: "[{LH3}{LH}]4- [{LH3}{LH}]{LH} {LH3}2" }]
+            },
+            "Pop 17": {
+                alias: "p17",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 [{RH}]2 [{RH}]3 {RH5}", desenhoLh: "[{LH3}{LH}]4 {LH3}2 z2" }]
+            },
+            "Pop 18": {
+                alias: "p18",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 {RH1}[{RH}] [{RH}]2 [{RH}]2", desenhoLh: "[{LH}{LH}]3 {LH}- {LH}2 {LH}{LH}" }]
+            },
+            "Pop 19": {
+                alias: "p19",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 {RH5}4- {RH5}{RH5}", desenhoLh: "{LH}3 {LH} z4" }]
+            },
+            "Pop 20": {
+                alias: "p20",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 [{RH}]4 {RH1}[{RH}]/", desenhoLh: "[{LH3}{LH5}]3 {LH}- {LH}4" }]
+            },
+            "Pop 21": {
+                alias: "p21",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 {RH1}2 [{RH}]2 [{RH}]>{RH5}", desenhoLh: "{LH3}4- {LH3}{LH3}/{LH5}/ {LH}/{LH5}[{LH}{LH3}]/" }]
+            },
+            "Pop 22": {
+                alias: "p22",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH1} {RH3}2 [{RH}] [{RH}]2 {RH5}2", desenhoLh: "{LH}3 {LH}- {LH}2 {LH}2" }]
+            },
+            "Pop 23": {
+                alias: "p23",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 z2 z4", desenhoLh: "[{LH}{LH}]2 z2 z4" }]
+            }
+        }
+    });
+
+    // ==========================================
+    // 3. ESTILO: SOLO PIANO POP WALTZ (p24 a p47) - 3/4
+    // ==========================================
+    REGISTRO_RITMOS.registrar({
+        categoria: "Pop 20",
+        subcategoria: "Ballad (3/4)",
+        style: "Solo Piano Pop Waltz",
+        variacoes: {
+            "Pop 24": {
+                alias: "p24",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH5}2 [{RH}] {RH1} {RH5}2", desenhoLh: "{LH}6" }]
+            },
+            "Pop 25": {
+                alias: "p25",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH5} {RH1} {RH5} {RH1} {RH5}2", desenhoLh: "{LH}6" }]
+            },
+            "Pop 26": {
+                alias: "p26",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH_A}]{RH3} [{RH_A}]2 [{RH_A}]{RH3}", desenhoLh: "{LH}4 E,2" }]
+            },
+            "Pop 27": {
+                alias: "p27",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH_A}]2 [{RH_A}] {RH1} (3{RH1}{RH3}{RH5}", desenhoLh: "[{LH}{LH5}]4 z2" }]
+            },
+            "Pop 28": {
+                alias: "p28",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 [{RH}] {RH5} [{RH}]2", desenhoLh: "{LH}2 z2 z2" }]
+            },
+            "Pop 29": {
+                alias: "p29",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH_A}]2 [{RH_A}] {RH3} [{RH_A}]2", desenhoLh: "[{LH}{LH5}]4- [{LH}{LH5}][{LH5}{RH3}]" }]
+            },
+            "Pop 30": {
+                alias: "p30",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH_A}]2 [{RH_A}]2 {RH_A}2", desenhoLh: "{LH}3 E z2" }]
+            },
+            "Pop 31": {
+                alias: "p31",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH_A}]2 [{RH}]2 [{RH}]2", desenhoLh: "[{LH}{LH5}]6" }]
+            },
+            "Pop 32": {
+                alias: "p32",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 [{RH5}]2 (3:2:2[{RH3}] {RH1}2", desenhoLh: "[{LH}{LH5}]4 {LH}{LH5}" }]
+            },
+            "Pop 33": {
+                alias: "p33",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH_A}]2 [{RH_A}]2 [{RH_A}]{RH1}", desenhoLh: "[{LH}{LH5}]6" }]
+            },
+            "Pop 34": {
+                alias: "p34",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH_A}]2 [{RH_A}]{RH1} [{RH_A}]{RH1}", desenhoLh: "[{LH}{LH5}]4 z2" }]
+            },
+            "Pop 35": {
+                alias: "p35",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH5}]2 [{RH}] {RH1} (3{RH5}{RH1}{RH3}", desenhoLh: "[{LH}{LH}]4 {LH}2" }]
+            },
+            "Pop 36": {
+                alias: "p36",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}] {RH5} [{RH}]2 [{RH}]2", desenhoLh: "[{LH}{LH}]3 {LH} z2" }]
+            },
+            "Pop 37": {
+                alias: "p37",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH_A}]2 [{RH_A}]2 [{RH_A}]{RH1}", desenhoLh: "[{LH}{LH5}]6" }]
+            },
+            "Pop 38": {
+                alias: "p38",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH_A}]2 [{RH_A}]{RH1} [{RH_A}]2", desenhoLh: "[{LH}{LH5}]4- [{LH}{LH5}]{RH1}" }]
+            },
+            "Pop 39": {
+                alias: "p39",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 (3[{RH}]{RH1}{LH5} [{RH}]2", desenhoLh: "[{LH}{LH}]6" }]
+            },
+            "Pop 40": {
+                alias: "p40",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 [{RH5}]2 [{RH}]{LH5}", desenhoLh: "[{LH}{LH}]6" }]
+            },
+            "Pop 41": {
+                alias: "p41",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 [{RH}]2 [{RH5}]{LH5}", desenhoLh: "[{LH}{LH}]6" }]
+            },
+            "Pop 42": {
+                alias: "p42",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 [{RH5}]{LH5} [{RH}]2", desenhoLh: "[{LH}{LH}]2 z2 z2" }]
+            },
+            "Pop 43": {
+                alias: "p43",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 [{RH}] {RH3} {RH5}2", desenhoLh: "[{LH}{LH}]4- [{LH}{LH}]{LH3}" }]
+            },
+            "Pop 44": {
+                alias: "p44",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 [{RH}]2 {RH_A}2", desenhoLh: "{LH}3 {LH} z2" }]
+            },
+            "Pop 45": {
+                alias: "p45",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 [{RH}]{RH3} {RH5}2", desenhoLh: "[{LH}]4 {LH3}2" }]
+            },
+            "Pop 46": {
+                alias: "p46",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}] {RH3} ({RH3}{RH1}{RH5} [{RH_A}]2", desenhoLh: "[{LH}]2 {LH}2 {LH5}2" }]
+            },
+            "Pop 47": {
+                alias: "p47",
+                metro: "3/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "(3:2:2[{RH}] {RH3}2- {RH3}4", desenhoLh: "[{LH}]6" }]
+            }
+        }
+    });
+
+
+    // ==========================================
+    // 4. ESTILO: SOLO ACOUSTIC PIANO BALLAD 
+    // ==========================================
+    REGISTRO_RITMOS.registrar({
+        categoria: "Pop 20",
+        subcategoria: "Lite Pop",
+        style: "Solo Acoustic Piano Ballad",
+        variacoes: {
+            "Pop 48": {
+                alias: "p48",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "z {RH1} {RH3}{RH5} {RH1}{RH3}{RH1}{RH5}-", desenhoLh: "[{LH}{LH5}]8" }]
+            },
+            "Pop 49": {
+                alias: "p49",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH5}2 {RH1}[{RH}] {RH3} {RH1}2 {RH3}-", desenhoLh: "{LH} {LH5}3- {LH5}2 {LH}2" }]
+            },
+            "Pop 50": {
+                alias: "p50",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH3}{RH1}{RH3}{RH5} {RH1}{RH3}{RH1}[{RH}]", desenhoLh: "[{LH}{LH5}]8" }]
+            },
+            "Pop 51": {
+                alias: "p51",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}][{RH}]{RH3}{RH1} {RH3}{RH5}{RH1}{RH3}", desenhoLh: "{LH}8" }]
+            },
+            "Pop 52": {
+                alias: "p52",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH_A} {RH1}2 {RH3}- {RH3}2 {RH1}[{RH_A}]-", desenhoLh: "{LH}2 {LH}2 {LH} {LH}3" }]
+            },
+            "Pop 53": {
+                alias: "p53",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH_A}]{RH5}{RH1}{RH3}- {RH3}{RH5} z2", desenhoLh: "{LH}4 {LH}2 {LH5}{LH}" }]
+            },
+            "Pop 54": {
+                alias: "p54",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH1}{RH5}{RH1}[{RH_A}]- [{RH_A}][{RH3}]{RH5}[{RH_A}]-", desenhoLh: "{LH}4 {LH5}2 z2" }]
+            },
+            "Pop 55": {
+                alias: "p55",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH_A}]{RH5}{RH1}[{RH_A}] [{RH3}] [{RH_A}]2 {RH3}", desenhoLh: "[{LH}{LH5}{LH}]3 {LH5} {LH}2 {LH}2" }]
+            },
+            "Pop 56": {
+                alias: "p56",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "z {RH1} {RH3}[{RH_A}] {RH3}{RH1}[{RH3}]{RH1}-", desenhoLh: "{LH}4 {LH5}4" }]
+            },
+            "Pop 57": {
+                alias: "p57",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH1}{RH5}[{RH1}]{RH5}- {RH5}{RH5}{RH3}{RH1}-", desenhoLh: "{LH}4 {LH5}4" }]
+            },
+            "Pop 58": {
+                alias: "p58",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH1}2 {RH1}[{RH3}] {RH1}[{RH3}] {RH1}2-", desenhoLh: "{LH} {LH}3- {LH}3 {LH5}" }]
+            },
+            "Pop 59": {
+                alias: "p59",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH1}2 [{RH1}]{RH1} [{RH}]{RH1} {RH1}2-", desenhoLh: "{LH} {LH}2 {LH5}- {LH5}{LH} {LH} z2" }]
+            },
+            "Pop 60": {
+                alias: "p60",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH1}2 [{RH1}]{RH1} [{RH}]{RH1} {RH1}2", desenhoLh: "{LH} {LH}3- {LH}3 {LH5}" }]
+            },
+            "Pop 61": {
+                alias: "p61",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "z {RH1} {RH3}{RH1} {RH1}2 {RH1}{RH5}-", desenhoLh: "[{LH}{LH}]4- [{LH}{LH}] {LH}3" }]
+            },
+            "Pop 62": {
+                alias: "p62",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH1}2 [{RH}][{RH3}] [{RH_A}][{RH3}]{RH1}[{RH3}]", desenhoLh: "{LH}{LH5}{LH}{LH}- {LH}{RH1} {RH3}2" }]
+            },
+            "Pop 63": {
+                alias: "p63",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]2 {RH1}{RH3} [{RH_A}]{RH3}[{RH3}]{RH3}", desenhoLh: "{LH} {LH5}3- {LH5} {LH}2 {RH1}" }]
+            },
+            "Pop 64": {
+                alias: "p64",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "{RH1} {RH3}2 {RH5}- {RH5}2 {RH3}{RH1}", desenhoLh: "[{LH}{LH}]2 {LH}2 {LH1}{LH3} z2" }]
+            },
+            "Pop 65": {
+                alias: "p65",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "z4 [{RH1}]>[{RH3}]- [{RH3}]<[{RH1}]", desenhoLh: "{LH}{LH5}{LH}{LH} {LH}>{LH_A}- {LH_A}<{LH}" }]
+            },
+            "Pop 66": {
+                alias: "p66",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "z2 [{RH1}]2- [{RH1}]{RH5}{RH3}{RH1}-", desenhoLh: "{LH} {LH}2 {LH} {LH5}4" }]
+            },
+            "Pop 67": {
+                alias: "p67",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]3{RH5}{RH1} [{RH3}]3 [{RH3}]", desenhoLh: "{LH}4- {LH}{LH} {LH5}2" }]
+            },
+            "Pop 68": {
+                alias: "p68",
+                metro: "4/4",
+                duracao: "1/8",
+                compassos: [{ desenhoRh: "[{RH}]8", desenhoLh: "[{LH}{LH5}{LH}]8" }]
+            }
+        }
+    });
+})();
